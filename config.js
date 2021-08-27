ee0f1ef6e0d20724a8852e69d2ae4a6d3ce58b53
Eivind Versvik <eivind_versvik@msn.com>
2021-08-27 16:04:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const config = {
     endpoint: "https://eivinddb.documents.azure.com:443/",
-    key: "K14oYzFZjKaVGg2UF20BqYzFqF9Rz3ycxRNpgzicPv2HiAykS0az4zzc2OSYpAzJBAxmma8J68PuE7nfnG4sLw==",
+    key: "",
     databaseId: "ToDoList",
     containerId: "Items",
     partitionKey: { kind: "Hash", paths: ["/id"] }
